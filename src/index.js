644fb0379e09e24f547d7b7e0a1624050c426456
rinarinalinux <rinarinalinux@gmail.com>
2020-05-23 20:19:11 +0900

    テーブル内容を動的に表示

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import './garbage.png';
 
 const initialize = () => {
     const date = document.getElementById('date');
+    console.log(date);
     const today = new Date();
-    
+    const data = [
+        {
+            place: '洗面所',
+            lastDate: '2020/3/20',
+            cycle: 14
+        },
+        {
+            place: 'キッチン',
+            lastDate: '2020/4/15',
+            cycle: 21
+        },
+        {
+            place: 'トイレ',
+            lastDate: '2020/4/19',
+            cycle: 7
+        },
+        {
+            place: '床',
+            lastDate: '2020/5/2',
+            cycle: 2
+        }
+    ];
+
     for (let i = 0; i < 10; i++) {
         const d = new Date(today.getTime() + (i - 7) * 24 * 60 * 60 * 1000 /* + (i - 7)日 */);
         const option = document.createElement('option');
@@ -14,6 +37,12 @@ const initialize = () => {
         }
         date.appendChild(option);
     }
+    const items = document.getElementById('items');
+    data.forEach(d => {
+        const tr = document.createElement('tr');
+        items.appendChild(tr);
+        tr.innerHTML = `<td>${d.place}</td><td>${d.lastDate}</td><td><input type="checkbox" name="finished"></td><td>${d.cycle}日</td><td><input type="checkbox" name="remove"></td>`;
+    });
 
     const button = document.getElementById('register');
     const onClick = () => {
